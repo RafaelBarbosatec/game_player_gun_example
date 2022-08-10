7c85b922d55e5559d60a72592e1dd52b742182cf
Rafael Barbosa <rafael@usedrip.com.br>
2022-08-10 15:32:12 -0300

    update bonfire

--- lib/player/hand_gun.dart
+++ lib/player/hand_gun.dart
@@ -1,13 +1,11 @@
 // ignore_for_file: file_names
 
 import 'package:bonfire/bonfire.dart';
-import 'package:flutter/widgets.dart';
 
 import 'kevin_spritesheet.dart';
 
 class HandGun extends GameComponent with UseSpriteAnimation {
   late SimplePlayer followerTarget;
-  AnimatedObjectOnce? _fastAnimation;
   late Vector2 _vectorRight;
   late Vector2 _vectorLeft;
   HandGun(Vector2 size) {
@@ -24,44 +22,15 @@ class HandGun extends GameComponent with UseSpriteAnimation {
     Vector2 aditionalPosition = isFlipHorizontal ? _vectorLeft : _vectorRight;
     position = followerTarget.position + aditionalPosition;
 
-    _fastAnimation?.position = position;
-    _fastAnimation?.update(dt);
     super.update(dt);
   }
 
-  @override
-  void render(Canvas canvas) {
-    if (_fastAnimation != null) {
-      _fastAnimation?.render(canvas);
-    } else {
-      super.render(canvas);
-    }
-  }
-
   void playShotAnimation() {
-    _playOnce(KevinSpriteSheet.gunRightShot);
+    playSpriteAnimationOnce(KevinSpriteSheet.gunRightShot);
   }
 
   void playReloadAnimation() {
-    _playOnce(KevinSpriteSheet.gunRightReload);
-  }
-
-  Future _playOnce(
-    Future<SpriteAnimation> animation, {
-    VoidCallback? onFinish,
-  }) async {
-    final anim = AnimatedObjectOnce(
-      position: position,
-      size: size,
-      animation: animation,
-      onFinish: () {
-        onFinish?.call();
-        _fastAnimation = null;
-      },
-    );
-    anim.gameRef = gameRef;
-    await anim.onLoad();
-    _fastAnimation = anim;
+    playSpriteAnimationOnce(KevinSpriteSheet.gunRightReload);
   }
 
   @override
